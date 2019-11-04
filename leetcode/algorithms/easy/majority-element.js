ab2e778c624d1c7c5a45370f8cfe9d2d5af8cb61
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2019-11-04 08:14:29 +0530

    feat: add js solution to leetcode problem - majority element

---
+++ leetcode/algorithms/easy/majority-element.js
@@ -0,0 +1,25 @@
+/*
+    leetcode
+    category: "Algorithms"
+    problem title: "Climbing Stairs"
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    
+    const elementsMap = {},halfLength = Math.floor(nums.length/2);
+    for(let i=0;i<nums.length;i++){
+        if(!elementsMap[nums[i]]){
+            elementsMap[nums[i]] = 1;
+        } else {
+            ++elementsMap[nums[i]];
+        }
+        if(elementsMap[nums[i]]>halfLength){
+            return nums[i];
+        }
+    }
+    
+};
