1dabd2c2ca1dd0e410b411e505885a2db01cf559
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2019-11-18 09:32:27 +0530

    feat: add js solution to leetcode problem - sum of two integers

---
+++ leetcode/algorithms/easy/sum-of-two-integers.js
@@ -0,0 +1,21 @@
+/*
+    leetcode
+    category: "Algorithms"
+    problem title: "Sum of Two Integers"
+*/
+
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+var getSum = function(a, b) {
+    
+    while(b!=0){
+        let carry = a&b;
+        a = a^b;
+        b = carry<<1;
+    }
+    return a;
+    
+};
