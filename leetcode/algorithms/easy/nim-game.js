be434d2e263a5e3177aaefa5b06c8c338df4731b
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2019-11-28 21:25:53 +0530

    feat: add js solution to leetcode problem - nim game

---
+++ leetcode/algorithms/easy/nim-game.js
@@ -0,0 +1,16 @@
+/*
+	leetcode
+	category: "Algorithms"
+	problem title: "Nim Game"
+*/
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var canWinNim = function(n) {
+    
+    
+    return n%4!=0;
+    
+};
