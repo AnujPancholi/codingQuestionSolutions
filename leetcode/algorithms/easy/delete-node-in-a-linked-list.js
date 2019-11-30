1cc1c808968c8c00726ac568fb851805a9db78ab
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2019-11-30 18:36:43 +0530

    feat: add js solution to leetcode problem - delete node in a linked list

---
+++ leetcode/algorithms/easy/delete-node-in-a-linked-list.js
@@ -0,0 +1,21 @@
+/*
+    leetcode
+    category: "Algorithms"
+    problem title: "Delete Node in a Linked List"
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function(node) {
+    node.val = node.next.val;
+    node.next = node.next.next;
+};
