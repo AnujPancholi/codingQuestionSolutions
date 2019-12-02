6684de63cc40dba61cf5dc6903063a80c7913f24
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2019-12-02 09:57:27 +0530

    feat: add js solution to leetcode problem - reverse vowels of a string

---
+++ leetcode/algorithms/easy/reverse-vowels-of-a-string.js
@@ -0,0 +1,34 @@
+/*
+    leetcode
+    category: "Algorithms"
+    problem title: "Reverse Vowels of a String"
+*/
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseVowels = function(s) {
+    const vowelSet = new Set(['a','e','i','o','u','A','E','I','O','U']);
+    const vowels = [],consonants = [],vowelIndices = [];
+    for(let i=0;i<s.length;i++){
+        if(vowelSet.has(s[i])){
+            vowels.push(s[i]);
+            vowelIndices.push(i);
+        } else {
+            consonants.push(s[i]);
+        }
+    }
+    vowels.reverse();
+    let newString='',v=0,c=0;
+    for(let i=0;i<s.length;i++){
+        if(vowelIndices[v]===i){
+            newString+=vowels[v];
+            ++v;
+        } else {
+            newString+=consonants[c];
+            ++c;
+        }
+    }
+    return newString;
+};
