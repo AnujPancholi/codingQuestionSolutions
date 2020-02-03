a9db0f35e992beb2ee55ff392995b2f66146c125
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2020-02-03 10:22:02 +0530

    refactor: change js solution to stack-based approach to leetcode problem by making a stack object and adding methods to prototype - remove all adjacent duplicates in string

--- leetcode/algorithms/stack/remove-all-adjacent-duplicates-in-string.js
+++ leetcode/algorithms/stack/remove-all-adjacent-duplicates-in-string.js
@@ -8,18 +8,43 @@
  * @param {string} S
  * @return {string}
  */
+
+function Stack(){
+    this.stackArr = [];
+}
+
+Stack.prototype.push = function(val){
+    this.stackArr.push(val);
+}
+
+Stack.prototype.pop = function(){
+    if(this.stackArr.length===0){
+        return null;
+    }
+    let value = this.stackArr.splice(this.stackArr.length-1,1);
+    return value;
+}
+
+Stack.prototype.isEmpty = function(){
+    return this.stackArr.length===0;
+}
+
+Stack.prototype.peek = function(){
+    return this.stackArr.length===0 ? null : this.stackArr[this.stackArr.length-1];
+}
+
 var removeDuplicates = function(S) {
     
-    const stackArr = [];
+    const dupStack = new Stack();
     
     for(let i=0;i<S.length;++i){
-        if(stackArr.length===0 || stackArr[stackArr.length-1]!=S[i]){
-            stackArr.push(S[i]);
+        if(dupStack.isEmpty() || dupStack.peek()!=S[i]){
+            dupStack.push(S[i]);
         } else {
-            stackArr.pop();
+            dupStack.pop();
         }
     }
     
-    return stackArr.join("");
+    return dupStack.stackArr.join("");
     
 };
