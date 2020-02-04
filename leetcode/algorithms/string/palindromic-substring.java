b569b1235c6c281322a73a1d885d5a6c09292115
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2020-02-04 08:36:52 +0530

    feat: add java solution to leetcode problem - palindromic substrings

---
+++ leetcode/algorithms/string/palindromic-substring.java
@@ -0,0 +1,37 @@
+/*
+    leetcode
+    category: "Algorithms"
+    problem title: "Palindromic Subtring"
+*/
+
+class Solution {
+    
+    private boolean isPalindromic(String s){
+        if(s.length()==1){
+            return true;
+        }
+        boolean isPalin = true;
+        for(int i=0;i<(int)(s.length()/2);++i){
+            if(s.charAt(i)!=s.charAt(s.length()-i-1)){
+                isPalin=false;
+                break;
+            }
+        }
+        return isPalin;
+    }
+    
+    public int countSubstrings(String s) {
+        int substringCount = 0;
+        for(int i=0;i<s.length();++i){
+            for(int j=i+1;j<s.length()+1;++j){
+                boolean isPalindromicResult = isPalindromic(s.substring(i,j));
+                if(isPalindromicResult){
+                    ++substringCount;
+                }
+            }
+        }
+        
+        return substringCount;
+        
+    }
+}
