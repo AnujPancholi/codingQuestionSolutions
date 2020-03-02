7c594915dd98dac0d66650ecf4f134271d332d09
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2020-03-02 08:08:35 +0530

    refactor: improve alternative java solution to leetcode problem - perfect number

--- leetcode/algorithms/math/perfect-number-ALT1.java
+++ leetcode/algorithms/math/perfect-number-ALT1.java
@@ -13,15 +13,14 @@ class Solution {
         int sum=1;
         HashSet<Integer> factorSet = new HashSet<Integer>();
         int i=2;
-        while(i<(int)(num/2)){
+        int upperLimit = (int)(num/2);
+        while(i<upperLimit){
             if(num%i==0){
                 if(!factorSet.contains(i)){
                     factorSet.add(i);
                     sum+=i;
-                }
-                if(!factorSet.contains((int)(num/i))){
-                    factorSet.add((int)(num/i));
-                    sum+=(int)(num/i);
+                    upperLimit = (int)(num/i);
+                    sum+=upperLimit;
                 }
             }
             ++i;
