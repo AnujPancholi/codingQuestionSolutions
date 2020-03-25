235a338dd6403c8990b69d53d9d887d525ff950b
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2020-03-25 11:22:07 +0530

    refactor: significantly improve js solution to leetcode problem - compare strings by frequency of the smallest character

--- leetcode/algorithms/string/compare-strings-by-frequency-of-the-smallest-character.js
+++ leetcode/algorithms/string/compare-strings-by-frequency-of-the-smallest-character.js
@@ -11,18 +11,34 @@
  * @return {number[]}
  */
 
+/**
+ * @param {string[]} queries
+ * @param {string[]} words
+ * @return {number[]}
+ */
+
 const getFreq = (str) => {
     const charArr = str.split("");
     const minChar = String.fromCharCode(Math.min(...charArr.map(c => c.charCodeAt(0))));
-    return charArr.reduce((count,c) => {
-        return c===minChar ? count+1 : count;
-    },0);
+    return {
+            value: minChar,
+            freq: charArr.reduce((count,c) => {
+            return c===minChar ? count+1 : count;
+        },0)
+    }
+}
+
+const FString = function(str){
+    this.value = str;
+    this.smallestChar = getFreq(str);
 }
 
 var numSmallerByFrequency = function(queries, words) {
-    return queries.reduce((resArr,q) => {
-        resArr.push(words.reduce((n,w) => {
-            if(getFreq(w)>getFreq(q)){
+    const fQueries = queries.map(q => new FString(q));
+    const fWords = words.map(w => new FString(w));
+    return fQueries.reduce((resArr,q) => {
+        resArr.push(fWords.reduce((n,w) => {
+            if(w.smallestChar.freq>q.smallestChar.freq){
                 ++n;
             }
             return n;
