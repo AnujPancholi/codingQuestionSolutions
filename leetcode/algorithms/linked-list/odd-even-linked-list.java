dd9345a0a03be590ebd4690444ff03f8d5d3cc6f
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2020-04-02 17:46:06 +0530

    refactor: improve java solution to leetcode problem - odd even linked list

--- leetcode/algorithms/linked-list/odd-even-linked-list.java
+++ leetcode/algorithms/linked-list/odd-even-linked-list.java
@@ -15,47 +15,33 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        if(head==null || head.next==null || head.next.next==null){
+        if(head==null || head.next==null){
             return head;
         }
-        ListNode p=null,q=null,pt=null,qt=null,h1=null,h2=null,t1=null,t2=null;
-        while(true){
+        ListNode p=null,q=null,h1=null,h2=null,t1=null,t2=null;
+        while(head!=null){
             p=head;
-            q=head.next;
+            q=p.next;
             
-            if(p!=null && p.next!=null){
-                p.next=p.next.next;
-            }
-            if(q!=null && q.next!=null){
-                q.next = q.next.next;
-                head = q.next==null ? null : q.next.next;
-            }   else {
-                head = null;
-            }
-            
-            pt = p.next;
-            qt = q==null ? null : q.next;
-            if(pt!=null){
-                pt.next=null;
-            }
-            if(qt!=null){
-                qt.next=null;
-            }
-            
-            if(t1==null && t2==null){
+            if(h1==null && h2==null){
                 h1=p;
                 h2=q;
-                t1=pt;
-                t2=qt;
-            } else { 
+            } else {
                 t1.next=p;
                 t2.next=q;
-                t1= p.next==null ? p : p.next;
-                t2= q==null || q.next==null ? q : q.next;
+            }
+            t1=p;
+            t2=q;
+            
+            if(q==null){
+                head=null;
+            } else {
+                head = q.next;
             }
             
-            if(head==null){
-                break;
+            p.next=null;
+            if(q!=null){
+                q.next=null;
             }
         }
         t1.next=h2;
