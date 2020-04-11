1e539ae7ccbe83c0d91c43f76b9c0446436ed59b
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2020-04-11 20:05:37 +0530

    feat: add java solution to leetcode problem - remove zero sum consecutive nodes from linked list

---
+++ leetcode/algorithms/linked-list/remove-zero-sum-consecutive-nodes-from-linked-list.java
@@ -0,0 +1,64 @@
+/*
+    leetcode
+    category: "Algorithms"
+    problem title: "Remove Zero Sum Consecutive Nodes From Linked List"
+*/
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    private void printList(ListNode head){
+        ListNode p = head;
+        while(p!=null){
+            System.out.print(Integer.toString(p.val)+"->");
+        }
+        System.out.print('\n');
+    }
+    public ListNode removeZeroSumSublists(ListNode head) {
+        int requiredStart=0;
+        while(requiredStart!=-2){
+            requiredStart=-2;
+            HashMap<Integer,Integer> sumToIndexMap = new HashMap<Integer,Integer>();
+            ListNode walker = head;
+            int index=0,cumulativeSum=0;
+            int requiredEnd=-1;
+            while(walker!=null){
+                cumulativeSum+=walker.val;
+                Integer indexForSumInteger = sumToIndexMap.get(cumulativeSum);
+                if(indexForSumInteger!=null){
+                    requiredStart = indexForSumInteger.intValue();
+                    requiredEnd = index;
+                    break;
+                } else if(cumulativeSum==0){
+                    requiredStart=-1;
+                    break;
+                } else {
+                    sumToIndexMap.put(cumulativeSum,index);
+                }
+                ++index;
+                walker = walker.next;
+            }
+            if(requiredStart>=0){
+                index=0;
+                ListNode lastNode = walker;
+                walker=head;
+                while(index<requiredStart){
+                    walker=walker.next;
+                    ++index;
+                }
+                walker.next = lastNode.next;
+            } else if(requiredStart==-1){
+                head=walker.next;
+            }   
+        }
+        return head;
+        
+    }
+}
