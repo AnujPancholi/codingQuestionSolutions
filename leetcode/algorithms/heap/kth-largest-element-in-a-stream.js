4d8071136c068ebad26c972a4113d273eda4bd6f
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2020-04-18 19:42:27 +0530

    refactor: improve js solution to leetcode problem - kth largest element in a stream

--- leetcode/algorithms/heap/kth-largest-element-in-a-stream.js
+++ leetcode/algorithms/heap/kth-largest-element-in-a-stream.js
@@ -9,40 +9,41 @@
  * @param {number[]} nums
  */
 
-const MinHeap = function(){
+const Heap = function(compare){
 	this._heap = [];
+	this._compare = compare;
 }
 
-MinHeap.prototype.swapValues = function(i1,i2){
+Heap.prototype.swapValues = function(i1,i2){
 	const temp = this._heap[i1];
 	this._heap[i1] = this._heap[i2];
 	this._heap[i2] = temp;
 }
 
-MinHeap.prototype.printHeapArray = function(){
+Heap.prototype.printHeapArray = function(){
 	console.log(this._heap);
 }
 
-MinHeap.prototype.getParentIndex = function(index){
+Heap.prototype.getParentIndex = function(index){
 	return parseInt((index-1)/2);
 }
 
-MinHeap.prototype.getRightChildIndex = function(index){
+Heap.prototype.getRightChildIndex = function(index){
 	const rightChildIndex = parseInt((index*2)+2);
 	return rightChildIndex<this._heap.length ? rightChildIndex : null;
 }
 
-MinHeap.prototype.getLeftChildIndex = function(index){
+Heap.prototype.getLeftChildIndex = function(index){
 	const leftChildIndex = parseInt((index*2)+1);
 	return leftChildIndex<this._heap.length ? leftChildIndex : null;
 }
 
 
-MinHeap.prototype.heapifyUp = function(){
+Heap.prototype.heapifyUp = function(){
 	let currentIndex = this._heap.length - 1;
 	while(currentIndex>0){
 		const parentIndex = this.getParentIndex(currentIndex);
-		if(this._heap[parentIndex]>this._heap[currentIndex]){
+		if(this._compare(this._heap[currentIndex],this._heap[parentIndex])>0){
 			this.swapValues(parentIndex,currentIndex);
 			currentIndex = parentIndex;
 		} else {
@@ -51,14 +52,14 @@ MinHeap.prototype.heapifyUp = function(){
 	}
 }
 
-MinHeap.prototype.heapifyDown = function(){
+Heap.prototype.heapifyDown = function(){
 	let currentIndex = 0;
 	while(currentIndex<this._heap.length){
 		const leftChildIndex = this.getLeftChildIndex(currentIndex);
 		if(leftChildIndex){
 			const rightChildIndex = this.getRightChildIndex(currentIndex);
-			let candidateIndex = rightChildIndex && this._heap[rightChildIndex]<this._heap[leftChildIndex] ? rightChildIndex : leftChildIndex;
-			if(this._heap[candidateIndex]<this._heap[currentIndex]){
+			let candidateIndex = rightChildIndex && this._compare(this._heap[rightChildIndex],this._heap[leftChildIndex])>0 ? rightChildIndex : leftChildIndex;
+			if(this._compare(this._heap[candidateIndex],this._heap[currentIndex])>0){
 				this.swapValues(candidateIndex,currentIndex);
 				currentIndex = candidateIndex;
 			} else {
@@ -70,20 +71,20 @@ MinHeap.prototype.heapifyDown = function(){
 	}
 }
 
-MinHeap.prototype.getSize = function(){
+Heap.prototype.getSize = function(){
 	return this._heap.length;
 }
 
-MinHeap.prototype.peek = function(){
-    return this._heap[0];
+Heap.prototype.peek = function(){
+	return this._heap[0];
 }
 
-MinHeap.prototype.add = function(value){
+Heap.prototype.add = function(value){
 	this._heap.push(value);
 	this.heapifyUp();
 }
 
-MinHeap.prototype.poll = function(){
+Heap.prototype.poll = function(){
 	const extractedValue = this._heap[0] || null;
 	const lastValue = this._heap.pop();
 	if(this._heap.length!=0){
@@ -92,9 +93,8 @@ MinHeap.prototype.poll = function(){
 	}
 	return extractedValue;
 }
-
 var KthLargest = function(k, nums) {
-    this._minHeap = new MinHeap();
+    this._minHeap = new Heap((i,j) => (j-i));
     this.k=k;
     for(let i=0;i<nums.length;++i){
         this._minHeap.add(nums[i]);
