abb6a5948c34bcdd31e8ae13ef07398e396d1db2
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2020-04-19 11:14:12 +0530

    feat: add js solution to leetcode problem - sort characters by frequency

---
+++ leetcode/algorithms/heap/sort-characters-by-frequency.js
@@ -0,0 +1,113 @@
+/*
+    leetcode
+    category: "Algorithms"
+    problem title: "Sort Characters by Frequency"
+*/
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const Heap = function(compare){
+	this._heap = [];
+	this._compare = compare;
+}
+
+Heap.prototype.swapValues = function(i1,i2){
+	const temp = this._heap[i1];
+	this._heap[i1] = this._heap[i2];
+	this._heap[i2] = temp;
+}
+
+Heap.prototype.printHeapArray = function(){
+	console.log(this._heap);
+}
+
+Heap.prototype.getParentIndex = function(index){
+	return parseInt((index-1)/2);
+}
+
+Heap.prototype.getRightChildIndex = function(index){
+	const rightChildIndex = parseInt((index*2)+2);
+	return rightChildIndex<this._heap.length ? rightChildIndex : null;
+}
+
+Heap.prototype.getLeftChildIndex = function(index){
+	const leftChildIndex = parseInt((index*2)+1);
+	return leftChildIndex<this._heap.length ? leftChildIndex : null;
+}
+
+
+Heap.prototype.heapifyUp = function(){
+	let currentIndex = this._heap.length - 1;
+	while(currentIndex>0){
+		const parentIndex = this.getParentIndex(currentIndex);
+		if(this._compare(this._heap[currentIndex],this._heap[parentIndex])>0){
+			this.swapValues(parentIndex,currentIndex);
+			currentIndex = parentIndex;
+		} else {
+			break;
+		}
+	}
+}
+
+Heap.prototype.heapifyDown = function(){
+	let currentIndex = 0;
+	while(currentIndex<this._heap.length){
+		const leftChildIndex = this.getLeftChildIndex(currentIndex);
+		if(leftChildIndex){
+			const rightChildIndex = this.getRightChildIndex(currentIndex);
+			let candidateIndex = rightChildIndex && this._compare(this._heap[rightChildIndex],this._heap[leftChildIndex])>0 ? rightChildIndex : leftChildIndex;
+			if(this._compare(this._heap[candidateIndex],this._heap[currentIndex])>0){
+				this.swapValues(candidateIndex,currentIndex);
+				currentIndex = candidateIndex;
+			} else {
+				break;
+			}
+		} else {
+			break;
+		}
+	}
+}
+
+Heap.prototype.getSize = function(){
+	return this._heap.length;
+}
+
+Heap.prototype.peek = function(){
+	return this._heap[0];
+}
+
+Heap.prototype.add = function(value){
+	this._heap.push(value);
+	this.heapifyUp();
+}
+
+Heap.prototype.poll = function(){
+	const extractedValue = this._heap[0] || null;
+	const lastValue = this._heap.pop();
+	if(this._heap.length!=0){
+		this._heap[0] = lastValue;
+		this.heapifyDown();
+	}
+	return extractedValue;
+}
+
+var frequencySort = function(s) {
+    const charToFreqMap = new Map();
+    for(let i=0;i<s.length;++i){
+        const currentVal = charToFreqMap.get(s[i]);
+        charToFreqMap.set(s[i],(currentVal || 0)+1);
+    }
+    const mapIterator = charToFreqMap[Symbol.iterator]();
+    const maxHeap = new Heap((p1,p2) => (p1[1]-p2[1]));
+    for(let pair of mapIterator){
+        maxHeap.add(pair);
+    }
+    const pseudoStringBuilder = [];
+    while(maxHeap.getSize()>0){
+        const currentPair = maxHeap.poll();
+        pseudoStringBuilder.push(currentPair[0].repeat(currentPair[1]));
+    }
+    return pseudoStringBuilder.join('');
+};
