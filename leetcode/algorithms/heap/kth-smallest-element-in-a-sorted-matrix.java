cb04e00f85e8a5cd8034dc3a8a1b7df3227942fe
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2020-04-24 20:06:56 +0530

    feat: add java solution to leetcode problem - kth smallest element in a sorted matrix

---
+++ leetcode/algorithms/heap/kth-smallest-element-in-a-sorted-matrix.java
@@ -0,0 +1,27 @@
+/*
+    leetcode
+    category: "Algorithms"
+    problem title: "Kth Largest Element in an Array
+*/
+
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(new Comparator<Integer>(){
+            public int compare(Integer i1, Integer i2){
+                return i2-i1;
+            }
+        });
+        for(int i=0;i<matrix.length;++i){
+            for(int j=0;j<matrix[0].length;++j){
+                maxHeap.add(matrix[i][j]);
+                if(maxHeap.size()>k){
+                    maxHeap.poll();
+                }
+            }
+        }
+        
+        return maxHeap.peek();
+    
+    }
+}
