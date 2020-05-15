f67b73d4abd8c65a93c9adb5959115fe99cce169
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2020-05-15 14:20:43 +0530

    feat: add iterative java solution to leetcode problem - binary tree inorder traversal

---
+++ leetcode/algorithms/stack/binary-tree-inorder-traversal-ITERATIVE.java
@@ -0,0 +1,54 @@
+/*
+    leetcode
+    category: "Algorithms"
+    problem title: "Binary Tree Inorder Traversal"
+*/
+
+    //Iterative solution. Not too efficient or elegant, came up with it myself on the fly
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> inorderList = new ArrayList<Integer>();
+        Stack<TreeNode> nodeStack = new Stack<TreeNode>();
+        HashSet<TreeNode> visitedSet = new HashSet<TreeNode>();
+        
+        TreeNode currNode = root;
+        while(currNode!=null || !nodeStack.empty()){
+            
+            
+            nodeStack.push(currNode);
+            visitedSet.add(currNode);
+            
+            if(currNode.left!=null && !visitedSet.contains(currNode.left)){
+                currNode=currNode.left;
+            } else if(currNode.right!=null && !visitedSet.contains(currNode.right)){
+                nodeStack.pop();
+                inorderList.add(currNode.val);
+                currNode = currNode.right;
+            } else {
+                inorderList.add(currNode.val);
+                nodeStack.pop();
+                currNode = nodeStack.empty() ? null : nodeStack.pop();
+            }
+            
+        }
+        
+        return inorderList;
+    
+    }
+}
