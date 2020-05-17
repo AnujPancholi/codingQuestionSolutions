3e90f6f0afbde1c3cfcc0affd5a1bc2729a2c868
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2020-05-17 16:33:38 +0530

    refactor: improve java solution to leetcode problem - minimum remove to make valid parentheses

--- leetcode/algorithms/stack/minimum-remove-to-make-valid-parentheses.java
+++ leetcode/algorithms/stack/minimum-remove-to-make-valid-parentheses.java
@@ -9,29 +9,23 @@ class Solution {
     public String minRemoveToMakeValid(String s) {
         
         Stack<Integer> parenStack = new Stack<Integer>();
+        StringBuilder ansBuilder = new StringBuilder(s);
         
         for(int i=0;i<s.length();++i){
-            Integer topCharVal = null;
+            Character topChar = null, currentChar = (s.charAt(i));
             if(!parenStack.empty()){
-                topCharVal = (int)(s.charAt(parenStack.peek()));
+                topChar = s.charAt(parenStack.peek());
             }
-            if(topCharVal!=null && topCharVal==40 && (int)(s.charAt(i))-topCharVal==1){
+            if(topChar!=null && !Character.isLetter(currentChar) && (int)(currentChar)-(int)(topChar)==1){
                 parenStack.pop();
-            } else if(((int)(s.charAt(i)))==40 || ((int)(s.charAt(i))==41)){
+            } else if(!Character.isLetter(currentChar)){
                 parenStack.push(i);
             }
         }
         
-        HashSet<Integer> removeSet = new HashSet<Integer>();
         
         while(!parenStack.isEmpty()){
-            removeSet.add(parenStack.pop());
-        }
-        
-        StringBuilder ansBuilder = new StringBuilder("");
-        
-        for(int i=0;i<s.length();++i){
-            if(!removeSet.contains(i))ansBuilder.append(s.charAt(i));
+            ansBuilder.deleteCharAt(parenStack.pop());
         }
         
         return ansBuilder.toString();
