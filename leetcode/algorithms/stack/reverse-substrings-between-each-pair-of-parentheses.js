7f027720f185baa7251aa3f68c36b6f3427c8bc8
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2020-06-05 21:51:44 +0530

    feat: add js solution to leetcode problem - reverse substrings between parentheses

---
+++ leetcode/algorithms/stack/reverse-substrings-between-each-pair-of-parentheses.js
@@ -0,0 +1,69 @@
+/*
+    leetcode
+    category: "Algorithms"
+    problem title: "Reverse Substrings Between Each Pair of Parentheses"
+*/
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+
+const _private_stack_map = new WeakMap();
+
+const Stack = function(){
+	const _stackData = {
+		stack: []
+	}
+	_private_stack_map.set(this,_stackData);
+	
+}
+
+
+Stack.prototype.push = function(val){
+	let stackData = _private_stack_map.get(this);
+	stackData.stack.push(val);
+}
+
+Stack.prototype.pop = function(){
+	let stackData = _private_stack_map.get(this);
+	return stackData.stack.pop();
+}
+
+Stack.prototype.peek = function(){
+	let stackData = _private_stack_map.get(this);
+	return stackData.stack.length===0 ? null : stackData.stack[stackData.stack.length-1];
+}
+
+Stack.prototype.getSize = function(){
+	let stackData = _private_stack_map.get(this);
+	return stackData.stack.length;
+}
+
+var reverseParentheses = function(s) {
+    const charStack = new Stack();
+    const charBuff = [];
+    let strArr = [];
+    
+    for(let i=0;i<s.length;++i){
+        if(s.charAt(i)===')'){
+            while(charStack.peek()!='('){
+                charBuff.push(charStack.pop());
+            }
+            charStack.pop();
+            while(charBuff.length>0){
+                charStack.push(charBuff.shift());
+            }
+        } else {
+            charStack.push(s.charAt(i));
+        }
+    }
+    
+    while(charStack.peek()){
+        strArr.push(charStack.pop());
+    }
+    
+    return strArr.reverse().join('');
+    
+    
+};
