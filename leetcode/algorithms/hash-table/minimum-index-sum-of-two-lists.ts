9f6d5232ac4c91e8fb85591f5d11986866fd1a4b
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2020-10-22 21:00:42 +0530

    refactor: improve ts solution to leetcode problem - miniumum index sum of two lists

--- leetcode/algorithms/hash-table/minimum-index-sum-of-two-lists.ts
+++ leetcode/algorithms/hash-table/minimum-index-sum-of-two-lists.ts
@@ -3,7 +3,6 @@
     category: "Algorithms"
     problem title: "Minimum Index Sum of Two Lists"
 */
-
 function getElementToIndexHashmap(list: any[]){
     const hashMap: Map<any,number> = new Map<any,number>();
     
@@ -18,43 +17,24 @@ function findRestaurant(list1: string[], list2: string[]): string[] {
     
     const l1Map: Map<any,number> = getElementToIndexHashmap(list1);
     
-    interface NameEntry {
-        name: any,
-        indexSum: number
-    }
+    const nameEntriesMap: Map<number,Array<any>> = new Map<number,Array<any>>();
     
-    const nameEntries: NameEntry[] = [];
-    
+    let minSum: number = list1.length+list2.length;
     list2.forEach((name,index) => {
         const mapEntry = l1Map.get(name);
         
         if(typeof mapEntry === "number"){
-            nameEntries.push({
-                name: name,
-                indexSum: index+mapEntry
-            })
+            nameEntriesMap.set(mapEntry+index,(nameEntriesMap.get(mapEntry+index) || []).concat(name));
+            if(minSum>mapEntry+index){
+                minSum = mapEntry+index;
+            }
         }
     })
-    
-    nameEntries.sort((n,m) => n.indexSum-m.indexSum);
-    
-    const minSum: number = nameEntries[0].indexSum;
-    let i: number = 0;
-    
-    const commonNames: any[] = [];
-    
-    while(i<nameEntries.length){
-        if(nameEntries[i].indexSum>minSum){
-            break;
-        }
-        commonNames.push(nameEntries[i].name);
-        ++i;
-    }
     
     // console.log(nameEntries);
     
     
-    return commonNames;
+    return nameEntriesMap.get(minSum) || [];
 
 
 };
