2fd084fe75ed593e119a808ec880fa2a93126c38
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2021-02-04 22:26:22 +0530

    feat: add ts sol to codesignal problm - hasPathWithSum

---
+++ codesignal/interview-practice/Trees/hasPathWithSum.ts
@@ -0,0 +1,31 @@
+/*
+    codesignal
+    category: "interview-practice/Arrays"
+    problem title: "hasPathWithSum"
+*/
+
+//
+// Binary trees are already defined with this interface:
+// class Tree<T> {
+//   value: T;
+//   left: Tree<T>;
+//   right: Tree<T>;
+//
+//   constructor(value: T) {
+//     this.value = value;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+
+function hasPathWithGivenSum(t: Tree<number>, s: number): boolean {
+    if(!t){
+        return false;
+    }
+    
+    if(t.left===null && t.right===null){
+        return s===t.value;
+    }
+    
+    return (hasPathWithGivenSum(t.left,s-t.value)) || (hasPathWithGivenSum(t.right,s-t.value))
+}
