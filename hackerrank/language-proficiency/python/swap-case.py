669125b9655164808093363105f0e92a010e1333
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2021-05-17 11:30:23 +0530

    feat: add py sol to hr lang proficiency problem - swap case

---
+++ hackerrank/language-proficiency/python/swap-case.py
@@ -0,0 +1,13 @@
+def swapCaseForCharVal(v):
+    if v>=97 and v<=122:
+        v=v-32
+    elif v>=65 and v<=90:
+        v=v+32
+    return chr(v)
+def swap_case(s):
+    return "".join(map(swapCaseForCharVal,[ord(ch) for ch in s]))
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
