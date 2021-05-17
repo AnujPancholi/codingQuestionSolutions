254a7651f44a28544afcec2dcd3b9450739f6754
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2021-05-17 11:52:46 +0530

    refactor: py sol to hr lang proficiency problem - swap case

--- hackerrank/language-proficiency/python/swap-case.py
+++ hackerrank/language-proficiency/python/swap-case.py
@@ -5,7 +5,7 @@ def swapCaseForCharVal(v):
         v=v+32
     return chr(v)
 def swap_case(s):
-    return "".join(map(swapCaseForCharVal,[ord(ch) for ch in s]))
+    return "".join([swapCaseForCharVal(ord(ch)) for ch in s])
 
 if __name__ == '__main__':
     s = input()
