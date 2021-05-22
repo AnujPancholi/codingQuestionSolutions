9e2079fbda47ec62104de0ba443e3cda852f0ac0
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2021-05-22 10:48:31 +0530

    feat: add py sol to lc prob - range sum of bst

---
+++ leetcode/algorithms/tree/range-sum-of-bst.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def rangeSumBST(self, root, low, high):
+        """
+        :type root: TreeNode
+        :type low: int
+        :type high: int
+        :rtype: int
+        """
+        if root==None:
+            return 0;
+        sum = 0;
+        
+        if root.val<high:
+            sum=sum+self.rangeSumBST(root.right,low,high);
+        
+        if root.val>low:
+            sum=sum+self.rangeSumBST(root.left,low,high);
+        
+        if(root.val<=high and root.val>=low):
+            sum=sum+root.val
+        
+        return sum;
