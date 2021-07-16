0c85955ffec8a2763cb7057ad741226e3f79e0b4
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2021-07-16 23:08:31 +0530

    refactor: improve ts sol to lc prob - remove duplicates from sorted list ii

--- leetcode/algorithms/linked-list/remove-duplicates-from-sorted-list-ii.ts
+++ leetcode/algorithms/linked-list/remove-duplicates-from-sorted-list-ii.ts
@@ -14,17 +14,19 @@ function deleteDuplicates(head: ListNode | null): ListNode | null {
     if(head===null){
         return head;
     }
-    let duplicateVal = head.next && head.next.val===head.val ? head.val : null;
-    while(duplicateVal!==null && head!==null && head.val==duplicateVal){
-        const nextNode = head.next;
-        head.next = null;
-        head = nextNode;
-        duplicateVal =  head!==null && head.next!==null && head.next.val===head.val ? head.val : duplicateVal;
+    
+    let duplicateNumber = null;
+    while(head.next!==null && head.next.val===head.val){
+        duplicateNumber = head.val;
+        head.next = head.next.next;
     }
     
-    if(head){
-        head.next = deleteDuplicates(head.next);
+    if(head.val===duplicateNumber){
+        return deleteDuplicates(head.next)
     }
+    
+    head.next = deleteDuplicates(head.next);
+    
     
     return head;
 
