332aba26efedb01f7871a1e7cfefbdfb491842fd
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2022-01-17 23:04:53 +0530

    refactor: java sol to lc prob - remove linked list elemtns

--- leetcode/algorithms/linked-list/remove-linked-list-elements.java
+++ leetcode/algorithms/linked-list/remove-linked-list-elements.java
@@ -1,36 +1,33 @@
-/*
-    leetcode
-    category: "Algorithms"
-    problem title: "Remove Linked List Elements"
-*/
-
 /**
  * Definition for singly-linked list.
  * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode(int x) { val = x; }
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
-        ListNode p = head;
+        
         while(head!=null && head.val==val){
-            p=head;
-            head=head.next;
-            p.next=null;
+            head = head.next;
         }
-        p=head;
-        ListNode q = null;
-        while(p!=null && p.next!=null){
-            if(p.next.val==val){
-                q=p.next;
-                p.next = p.next.next;
-                q.next=null;
-            } else {
-                p=p.next;
+        
+        ListNode cursorNode = head;
+
+        while(cursorNode!=null){
+            if(cursorNode.next!=null && cursorNode.next.val==val){
+                ListNode nextNode = cursorNode.next;
+                while(nextNode!=null && nextNode.val==val){
+                    nextNode = nextNode.next;
+                }
+                cursorNode.next = nextNode;
             }
+            cursorNode = cursorNode.next;
         }
+        
         return head;
     }
 }
