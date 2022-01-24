c11e4ca964386b0ef330d7149bc6dae831421dd8
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2022-01-24 12:00:45 +0530

    feat: add java sol to lc prob - lowest common ancestor of BST

---
+++ leetcode/algorithms/tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+        if(root==null || root==p || root==q){
+            return root;
+        }
+        
+        TreeNode leftLca = lowestCommonAncestor(root.left,p,q);
+        TreeNode rightLca = lowestCommonAncestor(root.right,p,q);
+        
+        if(leftLca!=null && rightLca!=null){
+            return root;
+        }
+        
+        return leftLca==null ? (rightLca==null ? null : rightLca) : leftLca;
+        
+        
+    }
+}
