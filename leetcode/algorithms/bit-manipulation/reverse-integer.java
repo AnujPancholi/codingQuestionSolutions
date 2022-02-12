7ea4def2fce0c8dd5e64365ed4c70368d66f03af
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2022-02-12 23:04:34 +0530

    feat: add java sol to lc prob - reverse integer

---
+++ leetcode/algorithms/bit-manipulation/reverse-integer.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int reverse(int x) {
+        
+        Stack<Integer> iStk = new Stack<Integer>();
+        int multiplier = x<0 ? -1 : 1;
+        x = Math.abs(x);
+        while(x>0){
+            iStk.push(x%10);
+            x = x/10;
+        }
+        
+        int i=0;
+        long reversedSum = 0;
+        while(!iStk.isEmpty()){
+            reversedSum+=(iStk.pop()*Math.pow(10,i));
+            ++i;
+        }
+        
+        return (int)(reversedSum>Integer.MAX_VALUE ? 0 : multiplier*(int)(reversedSum));
+        
+    }
+}
