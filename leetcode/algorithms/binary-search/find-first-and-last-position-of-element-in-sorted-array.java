f3bc1aae5248a7caa0b98266deb3abbfb37d2e95
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2022-02-18 23:45:03 +0530

    feat: add java sol to lc prob - find first and last position of element in sorted array

--- leetcode/algorithms/binary-search/find-first-and-last-position-of-element-in-sorted-array.java
+++ leetcode/algorithms/binary-search/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,4 +1,36 @@
 class Solution {
+    
+    private int findFirstIndex(int[] nums, int l, int h, int target){
+        
+        while(l<=h){
+            int m = l + ((h-l)/2);
+            if(nums[m]==target && (m==0 || nums[m-1]!=target)){
+                return m;
+            } else if(nums[m]==target && nums[m-1]==target){
+                h = m-1;
+            } else {
+                l = m+1;
+            }
+        }
+        
+        return 0;
+    }
+    
+    private int findLastIndex(int[] nums,int l,int h,int target){
+        
+        while(l<=h){
+            int m = l + ((h - l)/2);
+            if(nums[m]==target && (m==nums.length-1 || nums[m+1]!=target)){
+                return m;
+            } else if(nums[m]==target && nums[m+1]==target){
+                l = m+1;
+            } else {
+                h = m-1;
+            }
+        }
+        return nums.length-1;
+    }
+    
     public int[] searchRange(int[] nums, int target) {
         
         int l = 0,h = nums.length-1;
@@ -21,18 +53,7 @@ class Solution {
             return new int[]{-1,-1};
         }
         
-        l = firstIndex;
-        h = firstIndex;
-        
-        while(h<nums.length && nums[h]==target){
-            ++h;
-        }
-        
-        while(l>=0 && nums[l]==target){
-            --l;
-        }
-        
-        return new int[]{l+1,h-1};
+        return new int[]{findFirstIndex(nums,0,firstIndex,target),findLastIndex(nums,firstIndex,nums.length-1,target)};
         
     }
 }
